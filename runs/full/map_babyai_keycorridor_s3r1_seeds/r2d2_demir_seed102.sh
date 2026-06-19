bed8a89af026fd1ea6e3c1d5bb5f8700cd1d17ac
Kajetan Frąckowiak <144358407+s28404@users.noreply.github.com>
2026-06-19 20:09:48 +0200

    Update r2d2_demir_seed102.sh

--- runs/full/map_babyai_keycorridor_s3r1_seeds/r2d2_demir_seed102.sh
+++ runs/full/map_babyai_keycorridor_s3r1_seeds/r2d2_demir_seed102.sh
@@ -6,7 +6,7 @@ python3 -u fine_tuned/minigrid/r2d2_run.py \
     --variant "r2d2_demir" \
     --env-id "BabyAI-KeyCorridorS3R1-v0" \
     --seed 102 \
-    --demir-beta1 0.0 \
-    --demir-beta2 1.0
+    --demir-beta1 0.7 \
+    --demir-beta2 0.3
 
 sleep 90
